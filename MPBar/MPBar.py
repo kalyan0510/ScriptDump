8456e4f97edb5b9ec41b380afefe9049b39e07bc
Kalyan G <gkalyan0510@gmail.com>
2024-05-28 16:54:50 -0400

    Update MPBar.py

--- MPBar/MPBar.py
+++ MPBar/MPBar.py
@@ -1,7 +1,10 @@
 import multiprocessing
 from tqdm import tqdm
 import time
-
+import sys
+import curses
+curses.setupterm()
+COLUMN_SIZE = curses.tigetnum('cols')
 class MPDisplay:
     _instance = None
     bars = {}
@@ -42,20 +45,34 @@ class MPDisplay:
                         pos = cls.next_position
                         cls.next_position += 1
                     cls.positions[bar_id] = pos
-                cls.bars[bar_id] = tqdm(total=value, desc=bar_id + '/' + str(pos), position=pos)
+                cls.bars[bar_id] = tqdm(total=value, desc=bar_id + '/' + str(pos), position=pos, file=sys.stdout)
             elif action == "update" and bar_id in cls.bars:
                 desc, value = value
                 if desc:
-                    cls.bars[bar_id].set_description(desc)
+                    cls.bars[bar_id].set_description(f'{bar_id} => {desc}')
                 else:
                     cls.bars[bar_id].set_description(f'=> Job:{bar_id}')
                 cls.bars[bar_id].update(value)
             elif action == "close" and bar_id in cls.bars:
-                cls.bars[bar_id].close()
                 with cls._lock:
+                    # mx_pos = -1
+                    # cpos =  cls.positions[bar_id]
+                    # for bid, pos in cls.positions.items():
+                    #     mx_pos = max(mx_pos, pos)
+                    #     if pos > cpos:
+                    #         cls.bars[bar_id].moveto(cls.positions[bar_id]-1)
+                    #         cls.positions[bar_id] = cls.positions[bar_id] - 1
+                    # cls.free_positions.add(mx_pos)
+                    # cls.bars[bar_id].moveto(0)
+                    cls.bars[bar_id].close()
+
                     cls.free_positions.add(cls.positions[bar_id])
                     del cls.bars[bar_id]
                     del cls.positions[bar_id]
+            elif action == "stat" and bar_id in cls.bars:
+                 cls.bars[bar_id].set_postfix(value)
+            elif action == "write" and bar_id in cls.bars:
+                 cls.bars[bar_id].write('\r' + " "*COLUMN_SIZE + "\r" + value)
 
     @classmethod
     def new_pbar(cls, bar_id, size):
@@ -67,30 +84,42 @@ class MPDisplay:
         cls.queue.put(("update", bar_id, (desc, value)))
 
     @classmethod
+    def write(cls, bar_id, value):
+        cls.queue.put(("write", bar_id, value))
+
+    @classmethod
+    def stat(cls, bar_id, value):
+        cls.queue.put(("stat", bar_id, value))
+
+    @classmethod
     def close_bar(cls, bar_id):
         cls.queue.put(("close", bar_id, 0))
 
     @classmethod
     def terminate(cls):
         cls.queue.put(None)
+        print("\r" + ' '*COLUMN_SIZE + '\rDONE\n')
+        print("\r" + ' '*COLUMN_SIZE + '\r\n')
+        print("\r" + ' '*COLUMN_SIZE + '\r\n')
         cls.listener_process.join()
-
-
-def code_run_in_multiprocess_env(params):
-    bar_id = MPDisplay.new_pbar(params['name'], params['size'])
-    for _ in range(params['size']):
-        time.sleep(0.002)  # Simulating work
-        MPDisplay.update(bar_id, 1)
-    MPDisplay.close_bar(bar_id)
 
 
 if __name__ == "__main__":
 
-    MPDisplay()
+    def code_run_in_multiprocess_env(params):
+        bar_id = MPDisplay.new_pbar(params['name'], params['size'])
+        for i in range(params['size']):
+            time.sleep(0.002)  # Simulating work
+            MPDisplay.update(bar_id, 1, desc=f"{i}")
+        MPDisplay.write(bar_id, f"done {bar_id}")
+        MPDisplay.stat(bar_id, {"stat":f"done {bar_id}"})
+        MPDisplay.close_bar(bar_id)
+
     MPDisplay.init()
+    # tqdm().set_description()
     # multiprocessing.set_start_method('fork')  # Required on some platforms like MacOS
-    params_list = [{'name': f'Job {i}', 'size': 1000 + i*100} for i in range(10)]
-    with multiprocessing.Pool(processes=3) as pool:
+    params_list = [{'name': f'Job {i}', 'size': 200 + x*100} for i, x in enumerate([3,2,1,4,2,5,6,3,1,3,6,3,12,4])]
+    with multiprocessing.Pool(processes=5) as pool:
         pool.map(code_run_in_multiprocess_env, params_list)
 
     MPDisplay.terminate()
